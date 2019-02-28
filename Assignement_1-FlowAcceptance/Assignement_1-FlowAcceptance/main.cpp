08c02f7fd7fd21b7192cfecd13ffb41554be5776
Joyce Chin <chinjoyce30@gmail.com>
2019-02-28 22:44:58 +0800

    update

--- Assignement_1-FlowAcceptance/Assignement_1-FlowAcceptance/main.cpp
+++ Assignement_1-FlowAcceptance/Assignement_1-FlowAcceptance/main.cpp
@@ -14,15 +14,32 @@
 using namespace std;
 
 
+int link[N][N];
+double cost[N][N];
+
 typedef struct{
     int acceptID;
     int flowPath[N];
 }ACCEPTPATH;
-ACCEPTPATH flowPath[N];
-int acceptFlows=0;
+ACCEPTPATH accept[N];
+int acceptFlows=0,throughPut=0;
 
-int checkFlow(int graph[N][N],int tail,int path[],int flowSize){
-    return **graph;
+void CheckandUpdate(int flowID,int path[],int tail,int flowSize){
+    int from,to,i;
+    from=path[0];
+    for(i=1;i<=tail;i++){
+        to=path[i];
+        if(link[from][to]-flowSize<0) return;
+    }
+    accept[acceptFlows].acceptID=flowID;
+    from=accept[acceptFlows].flowPath[0]=path[0];
+    for(i=1;i<=tail;i++){
+        to=accept[acceptFlows].flowPath[i]=path[i];
+        link[from][to]-=flowSize;
+        cost[from][to]=(double)flowSize/link[from][to];
+    }
+    throughPut+=flowSize;
+    acceptFlows++;
 }
 
 int CopyList(int from[],int to[],int tail,int end){
@@ -35,7 +52,7 @@ int choose(int dist[],int nodes,int visit[]){
     int mini=MAX_COST;
     int index=-1;
     for(int i=0;i<nodes;i++){
-        if(dist[i]<mini){
+        if(!visit[i] && dist[i]<mini){
             index=i;
             mini=dist[i];
         }
@@ -43,13 +60,13 @@ int choose(int dist[],int nodes,int visit[]){
     return index;
 }
 
-void ShortestPath(int graph[N][N],int nodes,int flowID,int sourceID,int destinationID, int flowSize){
+void ShortestPath(int nodes,int flowID,int sourceID,int destinationID, int flowSize){
     int visit[N]={0};
     int dist[N],path[N][N]={0},tail[N];
     int i;
     for(i=0;i<nodes;i++) tail[i]=-1;
     for(i=0;i<nodes;i++){
-        dist[i]=graph[sourceID][i];
+        dist[i]=cost[sourceID][i];
         if(dist[i]<MAX_COST){
             path[i][0]=sourceID;
             path[i][1]=i;
@@ -61,15 +78,15 @@ void ShortestPath(int graph[N][N],int nodes,int flowID,int sourceID,int destinat
         int next=choose(dist,nodes,visit);
         visit[next]=1;
         for(int u=0;u<nodes;u++){
-            if(!visit[u] && dist[next]+graph[next][u]<dist[u]){
-                dist[u]=dist[next]+graph[next][u];
+            if(!visit[u] && dist[next]+cost[next][u]<dist[u]){
+                dist[u]=dist[next]+cost[next][u];
                 memset(path[u],0,sizeof(path[u]));
                 *path[u]=CopyList(path[next],path[u],tail[next],u);
                 tail[u]=tail[next]+1;
             }
         }
     }
-    **graph=checkFlow(graph,tail[destinationID],path[destinationID],flowSize);
+    CheckandUpdate(flowID,path[destinationID],tail[destinationID],flowSize);
 }
 
 
@@ -82,20 +99,18 @@ int main()
     fout.open("result.txt");
     fin.open("request.txt");
     fin >> nodes >> directedLinks;
-    int graph[N][N];
     int i,j;
     for(i=0;i<N;i++){
-        for(j=0;j<N;j++) graph[i][j]=MAX_COST;
+        for(j=0;j<N;j++) cost[i][j]=MAX_COST;
     }
     while(directedLinks--){
         fin >> linkID >> firstNode >> secondNode >> linkCapacity;
-        graph[firstNode][secondNode]=linkCapacity;
+        link[firstNode][secondNode]=cost[firstNode][secondNode]=linkCapacity;
     }
     fin >> requestFlows;
     while(requestFlows--){
         fin >> flowID >> sourceID >> destinationID >> flowSize;
-        ShortestPath(graph,nodes,flowID,sourceID,destinationID,flowSize);
-        break;
+        ShortestPath(nodes,flowID,sourceID,destinationID,flowSize);
     }
     return 0;
 }
